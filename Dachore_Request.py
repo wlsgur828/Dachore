d004f598c6c63d7b83b645722d5472d91a35e065
wlsgur828 <51524368+wlsgur828@users.noreply.github.com>
2019-06-10 19:03:25 +0900

    Update Dachore_Request.py

--- Dachore_Request.py
+++ Dachore_Request.py
@@ -1,6 +1,12 @@
 import requests
+import os
 
-print (" Dachore Start ")
+# banner
+f=open("text.txt",'r')
+data=f.read()
+print(data)
+f.close()
+
 imageName = input ('plz imageName = ')
 url = 'http://13.209.35.206:8000/DachoreApp/'+imageName
 res = requests.get(url)
